ec2a53e2111898a6a70bc3527381bb48ed9205bd
Marrnia <martin.jivkov@outlook.com>
2020-11-01 16:49:30 +0200

    Modified

--- Week 3/Task_5.cpp
+++ Week 3/Task_5.cpp
@@ -1,39 +1,30 @@
 #include <iostream>
 
-bool isYearLeap(int y){
-	if (y % 4 == 0){
-		if (y % 100 == 0){
-			if (y % 400 == 0){
-				return true;
-			}
-			return false;
-		}
-		return true;
-	}
-	return false;
-}
-
 int main() {
 	int day = 0, month = 0, year = 0;
-	int count = 0;
-	int monthDays[12] = {31, 28, 31, 30, 31, 30, 
-       31, 31, 30, 31, 30, 31};
+	int pastDays = 0;
 	std::cin >> day >> month >> year;
-	if (day >= 0 && day <= 31 && month >= 0 && month <= 12) {
-		count += day;
-		month--;
-		if (!isYearLeap(year)){
-			while (month){
-				count += monthDays[--month];
-			}
+	bool isLeapYear = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+	if (day >= 0 && day <= 31) {
+		pastDays += day;
+		switch (month) {
+			case 12: pastDays += 30;
+			case 11: pastDays += 31;
+			case 10: pastDays += 30;
+			case 9: pastDays += 31;
+			case 8: pastDays += 31;
+			case 7: pastDays += 30;
+			case 6: pastDays += 31;
+			case 5: pastDays += 30;
+			case 4: pastDays += 31;
+			case 3:
+				if (isLeapYear) pastDays += 29;
+				else pastDays += 28;
+			case 2: pastDays += 31;
+			case 1: break;
+			default: std::cout << "Invalid input" << '\n';
 		}
-		else {
-			monthDays[1]++;
-			while (month){
-				count += monthDays[--month];
-			}
-		}
-		std::cout << count;
+		std::cout << pastDays << '\n';
 	} else std::cout << "Invalid input";
 	return 0;
 }
