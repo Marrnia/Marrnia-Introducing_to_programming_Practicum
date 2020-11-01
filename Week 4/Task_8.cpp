99ae8e0c152afb6ce1589587a3da64a2b2b80cf1
Marrnia <martin.jivkov@outlook.com>
2020-11-01 21:24:17 +0200

    Modified Task_8

--- Week 4/Task_8.cpp
+++ Week 4/Task_8.cpp
@@ -6,9 +6,9 @@ int main() {
 	if (num >= 0)
 		for (int row = 0; row < num; row++) {
 			for (int col = 0; col < num; col++) {
-				if (row == 0 || col == 0 || col == num-1 || row == num - 1) std::cout << "*";
-				else if (row == col || row < col) std::cout << " ";
-				else std::cout << "*";
+				if (row == 0 || col == 0 || col == num-1 || row == num - 1) std::cout << " * ";
+				else if (row == col || row < col) std::cout << "   ";
+				else std::cout << " * ";
 			}
 			std::cout << std::endl;
 		}
